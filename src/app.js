0d072ff427fe502f43f65a43f20fc197e4a6d0ec
Apoorva Gupta <Apoorva Gupta>
2021-05-04 16:12:12 +0530

    1 error update

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const hbs = require('hbs')
 const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 //Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
